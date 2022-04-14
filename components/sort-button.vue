4c7ecddbe68f099dd3ec28a6aca55bf5e7ee9c54
Nicole <67213981+nicole-code@users.noreply.github.com>
2022-04-14 19:06:39 -0400

    feat: Added toggle functionality on menu

--- components/sort-button.vue
+++ components/sort-button.vue
@@ -2,13 +2,12 @@
 
   <div class="col-10">
     <div class="dotted-border">
-      <button ref="dropdownButton" class="dropdown-button" @click="toggleDropdown()">
+      <button ref="dropdownButton" class="dropdown-button" @click="toggleDropdown">
         <span>Sort by</span>
       </button>
       <div
         ref="dropdownContent"
-        class="dropdown-content"
-        :class="'show' ? 'show' : ''">
+        :class="[show ? 'dropdown-content show' : 'dropdown-content']">
         
         <div class="dropdown-item">
           Renew By - Latest
@@ -26,14 +25,28 @@
 <script>
 // ===================================================================== Imports
 // ====================================================================== Export
+
 export default {
   name: 'SortButton',
 
-  methoods: {
-    toggleDropdown () {
+  data () {
+    return {
+      show: 'show'
+    }
+  },
 
-      //if we click on this button, then change the class of dropdowncontent to show 
-      this.$refs.dropdownContent.classList.add('show')
+  methods: {
+    toggleDropdown () {
+      // let element = this.$refs.dropdownContent
+      // if (element.classList.contains('show'))  element.classList
+      console.log('HEY LOOK ', this.show)
+      if (this.show === 'show') {
+        this.show = ''
+      } else {
+        this.show = 'show'
+      }
+      // if we click on this button, then change the class of dropdowncontent to show
+      // this.$refs.dropdownContent.classList.add('show')
     }
   }
 }
@@ -41,40 +54,40 @@ export default {
 
 <style lang="scss" scoped>
 // ///////////////////////////////////////////////////////////////////// General
-.dotted-border {
-  height: 3.1rem;
-  width:  12.7rem;
-  padding: 1.5rem;
-  margin: 1rem;
-  display: inline-block;
-  position: relative;
-  background-repeat: no-repeat;
-  overflow: hidden;
-  background-image: url("data:image/svg+xml,%3csvg width='100%25' height='100%25' xmlns='http://www.w3.org/2000/svg'%3E%3Cdefs%3E%3Cmask id='mask-top-left'%3E%3Crect x='0' y='0' width='100%25' height='100%25' fill='white' /%3E%3Crect x='0' y='0' width='80%25' height='10%25' fill='black' /%3E%3C/mask%3E%3C/defs%3E%3Crect x='-2' y='2' width='98%25' height='95%25' rx='8' fill='none' stroke='blue' stroke-width='3' stroke-dasharray='2, 14' stroke-dashoffset='10' stroke-linecap='round' mask='url(%23mask-top-left)'/%3E%3C/svg%3e");
-  &:before {
-    content: '';
-    position: absolute;
-    background-image: url("data:image/svg+xml,%3Csvg width='8' height='8' xmlns='http://www.w3.org/2000/svg'%3E%3Cellipse ry='4' rx='4' cy='4' cx='4' fill='%23001FE6'/%3E%3C/svg%3E");
-    background-size: contain;
-    background-repeat: no-repeat;
-    transform: translateX(-50%);
-    width: 0.3125rem;
-    height: 0.3125rem;
-    left: 26.2rem;
-  }
-  &:after {
-    content: '';
-    position: absolute;
-    background-image: url("data:image/svg+xml,%3Csvg width='8' height='8' xmlns='http://www.w3.org/2000/svg'%3E%3Cellipse ry='4' rx='4' cy='4' cx='4' fill='%23001FE6'/%3E%3C/svg%3E");
-    background-size: contain;
-    background-repeat: no-repeat;
-    transform: translateX(50%);
-    width: 0.3125rem;
-    height: 0.3125rem;
-    bottom: 0;
-    right: 0;
-  }
-}
+// .dotted-border {
+//   height: 3.1rem;
+//   width:  12.7rem;
+//   padding: 1.5rem;
+//   margin: 1rem;
+//   display: inline-block;
+//   position: relative;
+//   background-repeat: no-repeat;
+//   overflow: hidden;
+//   background-image: url("data:image/svg+xml,%3csvg width='100%25' height='100%25' xmlns='http://www.w3.org/2000/svg'%3E%3Cdefs%3E%3Cmask id='mask-top-left'%3E%3Crect x='0' y='0' width='100%25' height='100%25' fill='white' /%3E%3Crect x='0' y='0' width='80%25' height='10%25' fill='black' /%3E%3C/mask%3E%3C/defs%3E%3Crect x='-2' y='2' width='98%25' height='95%25' rx='8' fill='none' stroke='blue' stroke-width='3' stroke-dasharray='2, 14' stroke-dashoffset='10' stroke-linecap='round' mask='url(%23mask-top-left)'/%3E%3C/svg%3e");
+//   &:before {
+//     content: '';
+//     position: absolute;
+//     background-image: url("data:image/svg+xml,%3Csvg width='8' height='8' xmlns='http://www.w3.org/2000/svg'%3E%3Cellipse ry='4' rx='4' cy='4' cx='4' fill='%23001FE6'/%3E%3C/svg%3E");
+//     background-size: contain;
+//     background-repeat: no-repeat;
+//     transform: translateX(-50%);
+//     width: 0.3125rem;
+//     height: 0.3125rem;
+//     left: 26.2rem;
+//   }
+//   &:after {
+//     content: '';
+//     position: absolute;
+//     background-image: url("data:image/svg+xml,%3Csvg width='8' height='8' xmlns='http://www.w3.org/2000/svg'%3E%3Cellipse ry='4' rx='4' cy='4' cx='4' fill='%23001FE6'/%3E%3C/svg%3E");
+//     background-size: contain;
+//     background-repeat: no-repeat;
+//     transform: translateX(50%);
+//     width: 0.3125rem;
+//     height: 0.3125rem;
+//     bottom: 0;
+//     right: 0;
+//   }
+// }
 
 span {
   font-size: $fontSize_Mini;
