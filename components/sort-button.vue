567468d30feb64c492d30fb37e4c3d6558ed4542
Nicole <67213981+nicole-code@users.noreply.github.com>
2022-04-14 10:35:41 -0400

    feat: Adding CSS styling to dropdown items

--- components/sort-button.vue
+++ components/sort-button.vue
@@ -34,50 +34,41 @@ export default {
 
 <style lang="scss" scoped>
 // ///////////////////////////////////////////////////////////////////// General
-.dotted-border {
-  height: 3.1rem;
-  width:  12.7rem;
-  padding: 1.5rem;
-  margin: 1rem;
-  display: inline-block;
-  position: relative;
-  background-repeat: no-repeat;
-  overflow: hidden;
-  background-image: url("data:image/svg+xml,%3csvg width='100%25' height='100%25' xmlns='http://www.w3.org/2000/svg'%3E%3Cdefs%3E%3Cmask id='mask-top-left'%3E%3Crect x='0' y='0' width='100%25' height='100%25' fill='white' /%3E%3Crect x='0' y='0' width='80%25' height='10%25' fill='black' /%3E%3C/mask%3E%3C/defs%3E%3Crect x='-2' y='2' width='98%25' height='95%25' rx='8' fill='none' stroke='blue' stroke-width='3' stroke-dasharray='2, 14' stroke-dashoffset='10' stroke-linecap='round' mask='url(%23mask-top-left)'/%3E%3C/svg%3e");
-  &:before {
-    content: '';
-    position: absolute;
-    background-image: url("data:image/svg+xml,%3Csvg width='8' height='8' xmlns='http://www.w3.org/2000/svg'%3E%3Cellipse ry='4' rx='4' cy='4' cx='4' fill='%23001FE6'/%3E%3C/svg%3E");
-    background-size: contain;
-    background-repeat: no-repeat;
-    transform: translateX(-50%);
-    width: 0.3125rem;
-    height: 0.3125rem;
-    left: 26.2rem;
-  }
-  &:after {
-    content: '';
-    position: absolute;
-    background-image: url("data:image/svg+xml,%3Csvg width='8' height='8' xmlns='http://www.w3.org/2000/svg'%3E%3Cellipse ry='4' rx='4' cy='4' cx='4' fill='%23001FE6'/%3E%3C/svg%3E");
-    background-size: contain;
-    background-repeat: no-repeat;
-    transform: translateX(50%);
-    width: 0.3125rem;
-    height: 0.3125rem;
-    bottom: 0;
-    right: 0;
-  }
-}
-
-// select {
+// .dotted-border {
+//   height: 3.1rem;
+//   width:  12.7rem;
+//   padding: 1.5rem;
+//   margin: 1rem;
+//   display: inline-block;
 //   position: relative;
-//   font-size: 1rem;
-//   border: none;
-//   visibility: hidden;
-//   &:hover {
-//     visibility: visible;
+//   background-repeat: no-repeat;
+//   overflow: hidden;
+//   background-image: url("data:image/svg+xml,%3csvg width='100%25' height='100%25' xmlns='http://www.w3.org/2000/svg'%3E%3Cdefs%3E%3Cmask id='mask-top-left'%3E%3Crect x='0' y='0' width='100%25' height='100%25' fill='white' /%3E%3Crect x='0' y='0' width='80%25' height='10%25' fill='black' /%3E%3C/mask%3E%3C/defs%3E%3Crect x='-2' y='2' width='98%25' height='95%25' rx='8' fill='none' stroke='blue' stroke-width='3' stroke-dasharray='2, 14' stroke-dashoffset='10' stroke-linecap='round' mask='url(%23mask-top-left)'/%3E%3C/svg%3e");
+//   &:before {
+//     content: '';
+//     position: absolute;
+//     background-image: url("data:image/svg+xml,%3Csvg width='8' height='8' xmlns='http://www.w3.org/2000/svg'%3E%3Cellipse ry='4' rx='4' cy='4' cx='4' fill='%23001FE6'/%3E%3C/svg%3E");
+//     background-size: contain;
+//     background-repeat: no-repeat;
+//     transform: translateX(-50%);
+//     width: 0.3125rem;
+//     height: 0.3125rem;
+//     left: 26.2rem;
+//   }
+//   &:after {
+//     content: '';
+//     position: absolute;
+//     background-image: url("data:image/svg+xml,%3Csvg width='8' height='8' xmlns='http://www.w3.org/2000/svg'%3E%3Cellipse ry='4' rx='4' cy='4' cx='4' fill='%23001FE6'/%3E%3C/svg%3E");
+//     background-size: contain;
+//     background-repeat: no-repeat;
+//     transform: translateX(50%);
+//     width: 0.3125rem;
+//     height: 0.3125rem;
+//     bottom: 0;
+//     right: 0;
 //   }
 // }
+
 span {
   font-size: $fontSize_Mini;
   font-weight: $fontWeight_Semibold;
@@ -92,6 +83,7 @@ span {
 }
 
 .dropdown-button {
+  position: relative;
   cursor: pointer;
   border: none;
   background: transparent;
@@ -100,8 +92,26 @@ span {
 }
 
 .dropdown-content {
-  // position: absolute;
-  height: 0;
+  position: absolute;
+  width: 11.56rem;
+  height: 5.25rem;
+  background: linear-gradient(131.13deg, #F7F9FA 8.78%, #E2E8EF 94.22%);
+  border-radius: 0.3rem;
+  box-shadow: 0px 20px 70px rgba(169, 180, 203, 0.3), -3px -3px 0px #FFFFFF, 0px 3px 0px #D6DADF, inset 0px -20px 20px rgba(255, 255, 255, 0.2);
+  padding: 0.5rem;
 }
 
+.dropdown-item {
+  font-size: $fontSize_Mini;
+  font-weight: $fontWeight_Semibold;
+  line-height: 1.875rem;
+  &:hover {
+    background: #D5DBE6;
+    width: 100%;
+  }
+}
+
+.col-10 {
+  margin-bottom: 5rem;
+}
 </style>
