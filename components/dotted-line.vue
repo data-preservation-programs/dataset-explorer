6cf3ab1880e3561990b5de3c3759da2bbf89d650
Nicole <67213981+nicole-code@users.noreply.github.com>
2022-05-17 21:13:09 -0400

    refactor: continuing to abstract and test svg

--- components/dotted-line.vue
+++ components/dotted-line.vue
@@ -1,14 +1,10 @@
 <template>
-  <svg class="dotted-border-svg" width='100%' height='100%' xmlns='http://www.w3.org/2000/svg'>
-    <rect x='6' y='-5' 
-    width='102.5%' 
-    height='100.5%' 
-    fill='none' 
-    stroke='#001FE6' 
-    stroke-width='2' 
-    stroke-dasharray='1.5, 10' 
-    stroke-dashoffset='2 0' 
-    stroke-linecap='round'/>
+  <svg class="dotted-border-svg" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg">
+    <rect
+      fill="none"
+      stroke="#001FE6"
+      stroke-width="2"
+      stroke-linecap="round" />
   </svg>
 </template>
 
@@ -23,35 +19,22 @@ export default {
 // ///////////////////////////////////////////////////////////////////// General
 svg {
   position: relative;
-  border: 0 0 1rem 1rem;
-  background-repeat: no-repeat;
   overflow: visible;
   &:before {
     content: '';
     position: absolute;
     background-color: $classicBlue;
     border-radius: 50%;
-    transform: translateY(-50%);
     width: 0.3125rem;
     height: 0.3125rem;
-    left: .22rem;
-    @include tiny {
-      top: 0.25rem;
-    }
   }
   &:after {
     content: '';
     position: absolute;
     background-color: $classicBlue;
     border-radius: 50%;
-    transform: translateX(50%);
     width: 0.3125rem;
     height: 0.3125rem;
-    bottom: 0;
-    right: 0;
-    @include tiny {
-      right: 0.25rem;
-    }
   }
 }
 </style>
--- pages/test.vue
+++ pages/test.vue
@@ -1,5 +1,20 @@
 <template>
-  <DottedLine/>
+  <header id="header-index">
+    <div class="grid">
+      <div class="col-9_mi-12_sm-10">
+        <div class="header-wrapper">
+          <h1 class="heading">
+            I am job
+          </h1>
+
+          <h2 class="subheading">
+            I'm sorry the position has been filled
+          </h2>
+          <DottedLine />
+        </div>
+      </div>
+    </div>
+  </header>
 </template>
 
 <script>
@@ -15,5 +30,58 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+
+.dotted-border-svg {
+  width: 102.5%;
+  height: 100.5%;
+  stroke-dasharray: 1.5, 10;
+  stroke-dashoffset: 2, 0;
+  .rect {
+    x: 6;
+    y: -5;
+  }
+}
+
+.heading {
+  position: relative;
+  @include header;
+  margin: 1rem;
+  padding-left: 3rem;
+  padding-top: 0.563rem;
+  @include medium {
+    font-size: 7.75rem;
+  }
+  @include small {
+    font-size: 7rem;
+    padding-left: 2rem;
+  }
+  @include mini {
+    font-size: 6rem;
+    padding-left: 2rem;
+  }
+  @include tiny {
+    font-size: 3.123rem;
+    padding-left: 2rem;
+    padding-top: 0;
+  }
+}
+
+.subheading {
+  @include subheading;
+  line-height: 2.438rem;
+  margin: 1rem;
+  padding-left: 3.75rem;
+  padding-bottom: 2.2rem;
+  @include small {
+    padding-left: 2.375rem;
+  }
+  @include mini {
+    padding-left: 2.375rem;
+  }
+  @include tiny {
+    font-size: 1.25rem;
+    padding-left: 2.375rem;
+  }
+}
 
 </style>
